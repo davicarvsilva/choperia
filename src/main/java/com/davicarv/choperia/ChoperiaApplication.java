0f1dc12f8771508e5c3f754afd0c8fda13af355d
davicarv13 <davicarv13@gmail.com>
2021-07-31 20:47:23 -0300

    Implementada a instância da classe funcionario

--- src/main/java/com/davicarv/choperia/ChoperiaApplication.java
+++ src/main/java/com/davicarv/choperia/ChoperiaApplication.java
@@ -12,12 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.davicarv.choperia.domain.Barril;
 import com.davicarv.choperia.domain.Cliente;
 import com.davicarv.choperia.domain.Endereco;
+import com.davicarv.choperia.domain.Funcionario;
 import com.davicarv.choperia.domain.MarcaBarrilEnum;
 import com.davicarv.choperia.domain.Telefone;
 import com.davicarv.choperia.domain.TipoPessoa;
 import com.davicarv.choperia.domain.Usuario;
 import com.davicarv.choperia.repository.BarrilRepository;
 import com.davicarv.choperia.repository.ClienteRepository;
+import com.davicarv.choperia.repository.FuncionarioRepository;
 
 @SpringBootApplication
 public class ChoperiaApplication implements CommandLineRunner {
@@ -26,6 +28,8 @@ public class ChoperiaApplication implements CommandLineRunner {
 	private BarrilRepository barrilRepo;
 	@Autowired
 	private ClienteRepository clienteRepo;
+	@Autowired
+	private FuncionarioRepository funcionarioRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChoperiaApplication.class, args);
@@ -33,7 +37,7 @@ public class ChoperiaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// Usuario
+		// Usuario1
 		Usuario usuario = new Usuario();
 		usuario.setNomeUsuario("davicarv");
 		usuario.setSenha("123");
@@ -86,6 +90,45 @@ public class ChoperiaApplication implements CommandLineRunner {
 		barril.setFabricacao(fabricacao);
 		
 		barrilRepo.save(barril);
+		
+		// Usuario1
+		Usuario usuario2 = new Usuario();
+		usuario2.setNomeUsuario("silascarv");
+		usuario2.setSenha("123");
+		
+		// Funcionario
+		Funcionario f1 = new Funcionario();
+		f1.setNome("Silas");
+		f1.setCpfOuCnpj(TipoPessoa.PESSOA_FISICA);
+		f1.setEmail("silascarv@gmail.com");
+		f1.setSobrenome("Carvalho");
+		
+		Telefone t2 = new Telefone();
+		t2.setCodigoPais("55");
+		t2.setDdd("22");
+		t2.setNumero("998132013");
+		List<Telefone> telefonesCliente2 = new ArrayList<>();
+		telefonesCliente.add(t2);
+		
+		Endereco end2 = new Endereco();
+		end2.setRua("Rua das flores");
+		end2.setCidade("Campos");
+		end2.setEstado("RJ");
+		end2.setNumero(14);
+		end2.setBairro("Goyta");
+		Calendar dataNascimentoCliente2 = Calendar.getInstance();
+		dataNascimentoCliente.set(1999, 2, 16);
+		
+		List<Endereco> enderecosCliente2 = new ArrayList<>();
+		enderecosCliente.add(end);
+		
+		f1.setEnderecos(enderecosCliente2);
+		f1.setDataNascimento(dataNascimentoCliente2);
+		f1.setTelefones(telefonesCliente2);
+		f1.setUsuario(usuario2);
+		f1.setCargo("Entregador");
+		f1.setSalario(1500);
+		funcionarioRepo.save(f1);
 		
 	}
 }
