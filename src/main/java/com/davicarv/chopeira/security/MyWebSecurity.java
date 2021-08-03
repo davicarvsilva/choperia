0c8893630bf5fed393e3f25982c649497655aa46
davicarv13 <davicarv13@gmail.com>
2021-08-02 21:24:15 -0300

    Atualização do MyWebSecurity para definição dos Roles

--- src/main/java/com/davicarv/chopeira/security/MyWebSecurity.java
+++ src/main/java/com/davicarv/chopeira/security/MyWebSecurity.java
@@ -2,7 +2,9 @@ package com.davicarv.chopeira.security;
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +13,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class MyWebSecurity extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private FuncionarioDetailsService service;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().ignoringAntMatchers("/apirest/**")
+		.and()
+		.authorizeRequests()
+		.antMatchers("/apirest/**").hasRole("ADMIN")
+		.antMatchers("/files/**").hasAnyRole("FUNC", "ADMIN")
+		.antMatchers("/funcionarios/meusdados/**").hasAnyRole("ADMIN", "FUNC")
+		.antMatchers("/funcionarios").hasRole("ADMIN")
+		.antMatchers("/funcionarios/**").hasRole("ADMIN")
+		.antMatchers("/**").hasAnyRole("ADMIN","FUNC")
+		.antMatchers().authenticated()
+		.and()
+		.httpBasic()
+		.and()
+		.formLogin()
+		.usernameParameter("email");
+	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
